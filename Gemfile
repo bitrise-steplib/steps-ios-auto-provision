8e8c26502d5e10c833e1fb1cd379701c0ab87ff9
Krisztián Gödrei <godreikrisztian@gmail.com>
2017-11-13 14:36:31 +0100

    rubocop update

--- Gemfile
+++ Gemfile
@@ -1,6 +1,6 @@
 source 'https://rubygems.org'
 
-gem 'fastlane', :github => 'fastlane/fastlane', :branch => 'master'
+gem 'fastlane', github: 'fastlane/fastlane', branch: 'master'
 gem 'openssl'
 gem 'plist'
 gem 'xcodeproj'
--- keychain/keychain.rb
+++ keychain/keychain.rb
@@ -1,3 +1,5 @@
+require 'English'
+
 # KeychainHelper
 class KeychainHelper
   def initialize(keychain_path, keychain_password)
@@ -23,7 +25,7 @@ class KeychainHelper
     cmd = ['security', '-v', 'create-keychain', '-p', keychain_password, keychain_path].join(' ')
     log_debug("$ #{cmd}")
     out = `#{cmd}`
-    raise "#{cmd} failed, out: #{out}" unless $?.success?
+    raise "#{cmd} failed, out: #{out}" unless $CHILD_STATUS.success?
   end
 
   def import_certificate(path, passphrase)
@@ -31,7 +33,7 @@ class KeychainHelper
     cmd = ['security', 'import', path, '-k', @keychain_path, '-P', passphrase, '-A'].join(' ')
     log_debug("$ #{cmd}")
     out = `#{cmd}`
-    raise "#{cmd} failed, out: #{out}" unless $?.success?
+    raise "#{cmd} failed, out: #{out}" unless $CHILD_STATUS.success?
   end
 
   def set_key_partition_list_if_needed
@@ -40,28 +42,28 @@ class KeychainHelper
     cmd = ['sw_vers', '-productVersion'].join(' ')
     log_debug("$ #{cmd}")
     current_version = `#{cmd}`
-    raise "#{cmd} failed, out: #{current_version}" unless $?.success?
+    raise "#{cmd} failed, out: #{current_version}" unless $CHILD_STATUS.success?
 
     return if Gem::Version.new(current_version) < Gem::Version.new('10.12.0')
 
     cmd = ['security', 'set-key-partition-list', '-S', 'apple-tool:,apple:', '-k', @keychain_password, @keychain_path].join(' ')
     log_debug("$ #{cmd}")
     out = `#{cmd}`
-    raise "#{cmd} failed, out: #{out}" unless $?.success?
+    raise "#{cmd} failed, out: #{out}" unless $CHILD_STATUS.success?
   end
 
   def set_keychain_settings_default_lock
     cmd = ['security', '-v', 'set-keychain-settings', '-lut', '72000', @keychain_path].join(' ')
     log_debug("$ #{cmd}")
     out = `#{cmd}`
-    raise "#{cmd} failed, out: #{out}" unless $?.success?
+    raise "#{cmd} failed, out: #{out}" unless $CHILD_STATUS.success?
   end
 
   def list_keychains
     cmd = ['security', 'list-keychains'].join(' ')
     log_debug("$ #{cmd}")
     list = `#{cmd}`
-    raise "#{cmd} failed, out: #{list}" unless $?.success?
+    raise "#{cmd} failed, out: #{list}" unless $CHILD_STATUS.success?
 
     list.split("\n").map(&:strip).map { |e| e.gsub!(/\A"|"\Z/, '') }
   end
@@ -71,20 +73,20 @@ class KeychainHelper
     cmd = ['security', '-v', 'list-keychains', '-s'].concat(keychains).join(' ')
     log_debug("$ #{cmd}")
     out = `#{cmd}`
-    raise "#{cmd} failed, out: #{out}" unless $?.success?
+    raise "#{cmd} failed, out: #{out}" unless $CHILD_STATUS.success?
   end
 
   def set_default_keychain
     cmd = ['security', '-v', 'default-keychain', '-s', @keychain_path].join(' ')
     log_debug("$ #{cmd}")
     out = `#{cmd}`
-    raise "#{cmd} failed, out: #{out}" unless $?.success?
+    raise "#{cmd} failed, out: #{out}" unless $CHILD_STATUS.success?
   end
 
   def unlock_keychain
     cmd = ['security', '-v', 'unlock-keychain', '-p', @keychain_password, @keychain_path].join(' ')
     log_debug("$ #{cmd}")
     out = `#{cmd}`
-    raise "#{cmd} failed, out: #{out}" unless $?.success?
+    raise "#{cmd} failed, out: #{out}" unless $CHILD_STATUS.success?
   end
 end
