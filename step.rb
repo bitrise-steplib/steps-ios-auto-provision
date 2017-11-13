df9648e7aa33911d9565f3c69b291941627f4c7c
Krisztián Gödrei <godreikrisztian@gmail.com>
2017-11-13 12:43:52 +0100

    handle multiple certificates in p12 file

--- step.rb
+++ step.rb
@@ -1,5 +1,3 @@
-require 'openssl'
-
 require_relative 'log/log'
 require_relative 'project_helper/project_helper'
 require_relative 'http_helper/http_helper'
@@ -8,6 +6,7 @@ require_relative 'auto-provision/authenticator'
 require_relative 'auto-provision/generator'
 require_relative 'auto-provision/app_services'
 require_relative 'keychain/keychain'
+require_relative 'certificate_helper/certificate_helper'
 
 # CertificateInfo
 class CertificateInfo
@@ -116,45 +115,6 @@ def split_pipe_separated_list(list)
   list.split('|').map(&:strip)
 end
 
-def certificate_common_name(certificate)
-  common_name = certificate.subject.to_a.find { |name, _, _| name == 'CN' }[1]
-  common_name = common_name.force_encoding('UTF-8')
-  common_name
-end
-
-def certificate_team_id(certificate)
-  certificate.subject.to_a.find { |name, _, _| name == 'OU' }[1]
-end
-
-def find_certificate_info_by_identity(identity, certificate_infos)
-  certificate_infos.each do |certificate_info|
-    common_name = certificate_common_name(certificate_info.certificate)
-    return certificate_info if common_name.downcase.include?(identity.downcase)
-  end
-  nil
-end
-
-def find_certificate_infos_by_team_id(team_id, certificate_infos)
-  matching_certificate_infos = []
-  certificate_infos.each do |certificate_info|
-    org_unit = certificate_team_id(certificate_info.certificate)
-    matching_certificate_infos.push(certificate_info) if org_unit.downcase.include?(team_id.downcase)
-  end
-  matching_certificate_infos
-end
-
-def find_matching_codesign_identity_info(identity_name, team_id, certificate_infos)
-  if identity_name
-    certificate_info = find_certificate_info_by_identity(identity_name, certificate_infos)
-    return certificate_info if certificate_info
-  end
-
-  team_certificate_infos = find_certificate_infos_by_team_id(team_id, certificate_infos)
-  return team_certificate_infos[0] if team_certificate_infos.to_a.length == 1
-  log_details('no development certificate found') if team_certificate_infos.to_a.empty?
-  log_warning("#{team_certificate_infos.length} development certificate found") if team_certificate_infos.to_a.length > 1
-end
-
 begin
   # Params
   params = Params.new
@@ -239,10 +199,14 @@ begin
     path = download_to_tmp_file(url, "Certrificate#{idx}.p12")
     log_debug("certificate path: #{path}")
 
-    certificate_info = CertificateInfo.new
-    certificate_info.path = path
-    certificate_info.passphrase = passphrases[idx]
-    certificate_infos.push(certificate_info)
+    certificates = read_certificates(path, passphrases[idx])
+    certificates.each do |certificate|
+      certificate_info = CertificateInfo.new
+      certificate_info.path = path
+      certificate_info.passphrase = passphrases[idx]
+      certificate_info.certificate = certificate
+      certificate_infos.push(certificate_info)
+    end
   end
   log_done("#{certificate_infos.length} certificates downloaded")
   ###
@@ -253,29 +217,25 @@ begin
   development_certificate_infos = []
   production_certificate_infos = []
   certificate_infos.each do |certificate_info|
-    certificate_content = File.read(certificate_info.path)
-    log_debug("searching for Certificate (#{certificate_info.path})")
-    raise "Invalid certificate file #{certificate_info.path}: empty" if certificate_content.to_s.empty?
+    log_debug("searching for Certificate (#{certificate_common_name(certificate_info.certificate)})")
 
-    p12 = OpenSSL::PKCS12.new(certificate_content, certificate_info.passphrase)
-
-    portal_certificate = find_development_portal_certificate(p12.certificate)
+    portal_certificate = find_development_portal_certificate(certificate_info.certificate)
     if portal_certificate
       log_done("development Certificate found: #{portal_certificate.name}")
       raise 'multiple development certificates provided: step can handle only one development (and only one production) certificate' unless development_certificate_infos.empty?
 
-      certificate_info.certificate = p12.certificate
+      certificate_info.certificate = certificate_info.certificate
       certificate_info.portal_certificate = portal_certificate
       development_certificate_infos.push(certificate_info)
     end
 
-    portal_certificate = find_production_portal_certificate(p12.certificate)
+    portal_certificate = find_production_portal_certificate(certificate_info.certificate)
     next unless portal_certificate
 
     log_done("production Certificate found: #{portal_certificate.name}")
     raise 'multiple production certificates provided: step can handle only one production (and only one development) certificate' unless production_certificate_infos.empty?
 
-    certificate_info.certificate = p12.certificate
+    certificate_info.certificate = certificate_info.certificate
     certificate_info.portal_certificate = portal_certificate
     production_certificate_infos.push(certificate_info)
   end
