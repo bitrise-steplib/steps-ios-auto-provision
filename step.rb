15eadb9e1ccfad5d65a8f9029b5190c59cae9974
godrei <godreikrisztian@gmail.com>
2018-01-10 14:39:06 +0100

    step.yml update, bundle update, more logs

--- step.rb
+++ step.rb
@@ -153,10 +153,13 @@ begin
   certificate_infos = []
   certificate_urls.each_with_index do |url, idx|
     Log.debug("downloading certificate ##{idx + 1}")
+
     path = download_or_create_local_path(url, "Certrificate#{idx}.p12")
     Log.debug("certificate path: #{path}")
 
     certificates = read_certificates(path, passphrases[idx])
+    Log.debug("#{certificates.length} codesign identities included")
+
     certificates.each do |certificate|
       certificate_info = CertificateInfo.new(path, passphrases[idx], certificate)
       certificate_infos = append_if_latest_certificate(certificate_info, certificate_infos)
