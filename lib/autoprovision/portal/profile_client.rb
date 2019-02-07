6f6b041f5669b29275ecb65ed370eda77d510326
Gödrei Krisztián <godreikrisztian@gmail.com>
2019-02-07 13:43:13 +0100

    pr updates

--- lib/autoprovision/portal/profile_client.rb
+++ lib/autoprovision/portal/profile_client.rb
@@ -7,21 +7,47 @@ module Portal
   class ProfileClient
     @profiles = {}
 
-    def self.ensure_xcode_managed_profile(bundle_id, entitlements, distribution_type, portal_certificate, platform, min_profile_days_valid)
+    def self.ensure_xcode_managed_profile(bundle_id, entitlements, distribution_type, certificate, platform, min_profile_days_valid)
       profiles = ProfileClient.fetch_profiles(true, platform)
-      profile = ProfileClient.matching_profile(profiles, distribution_type, bundle_id, entitlements, portal_certificate, min_profile_days_valid)
 
-      unless profile
-        error_message = [
-          "Failed to find #{distribution_type} Xcode managed provisioning profile for bundle id: #{bundle_id}.",
-          'Please open your project in your local Xcode and generate and ipa file',
-          'with the desired distribution type and by using Xcode managed codesigning.',
-          'This will create / refresh the desired managed profiles.'
-        ].join("\n")
-        raise error_message
+      # Separate matching profiles
+      # full_matching_profiles contains profiles which bundle id equals to the provided bundle_id
+      # matching_profiles contains profiles which bundle id glob matches to the provided bundle_id
+      full_matching_profiles = []
+      matching_profiles = []
+      profiles.each do |profile|
+        if profile.app.bundle_id == bundle_id
+          full_matching_profiles.push(profile)
+          next
+        end
+
+        matching_profiles.push(profile) if File.fnmatch(profile.app.bundle_id, bundle_id)
       end
 
-      profile
+      profiles = full_matching_profiles.select do |profile|
+        distribution_type_matches?(profile, distribution_type) &&
+          !expired?(profile, min_profile_days_valid) &&
+          all_services_enabled?(entitlements, entitlements) &&
+          include_certificate?(profile, certificate)
+      end
+
+      return profiles[0] unless profiles.empty?
+
+      profiles = matching_profiles.select do |profile|
+        distribution_type_matches?(profile, distribution_type) &&
+          !expired?(profile, min_profile_days_valid) &&
+          all_services_enabled?(entitlements, entitlements) &&
+          include_certificate?(profile, certificate)
+      end
+
+      return profiles[0] unless profiles.empty?
+
+      raise [
+        "Failed to find #{distribution_type} Xcode managed provisioning profile for bundle id: #{bundle_id}.",
+        'Please open your project in your local Xcode and generate and ipa file',
+        'with the desired distribution type and by using Xcode managed codesigning.',
+        'This will create / refresh the desired managed profiles.'
+      ].join("\n")
     end
 
     def self.ensure_manual_profile(certificate, app, entitlements, distribution_type, platform, min_profile_days_valid, allow_retry = true)
@@ -30,68 +56,16 @@ module Portal
       # search for the Bitrise managed profile
       profile_name = "Bitrise #{distribution_type} - (#{app.bundle_id})"
       profile = all_profiles.select { |prof| prof.name == profile_name }.first
-      valid = !profile.nil?
 
-      # check the profile's bundle id
-      if valid
-        if profile.app.bundle_id != app.bundle_id
-          Log.debug("Profile (#{profile.name}) bundle id: #{profile.app.bundle_id}, should be: #{app.bundle_id}")
-          valid = false
-        end
-      end
-
-      # check the profile's distribution type
-      if valid
-        distribution_methods = {
-          'development' => 'limited',
-          'app-store' => 'store',
-          'ad-hoc' => 'adhoc',
-          'enterprise' => 'inhouse'
-        }
-        desired_distribution_method = distribution_methods[distribution_type]
-
-        unless profile.distribution_method == desired_distribution_method
-          Log.debug("Profile (#{profile.name}) distribution type: #{profile.distribution_method}, should be: #{desired_distribution_method}")
-          valid = false
-        end
-      end
-
-      # check the profile expiry
-      if valid
-        # Increment the current time with days in seconds (1 day = 86400 secs) the profile has to be valid for
-        expire = Time.now + (min_profile_days_valid * 86_400)
-
-        if Time.parse(profile.expires.to_s) < expire
-          if min_profile_days_valid > 0
-            Log.debug("Profile (#{profile.name}) is not valid for #{min_profile_days_valid} days")
-          else
-            Log.debug("Profile (#{profile.name}) expired at: #{profile.expires}")
-          end
-
-          valid = false
-        end
-      end
-
-      # check if project capabilities are enabled for the profile
-      if valid
-        unless AppClient.all_services_enabled?(profile.app, entitlements)
-          Log.debug("Profile (#{profile.name}) does not contain every required services")
-          valid = false
-        end
-      end
-
-      # check if the profile contains the given certificate
-      if valid
-        unless include_certificate?(profile, certificate)
-          Log.debug("Profile (#{profile.name}) does not contain certificate: #{certificate.name}")
-          valid = false
-        end
-      end
-
-      return profile if valid
+      return profile if !profile.nil? &&
+                        bundle_id_matches?(profile, app) &&
+                        distribution_type_matches?(profile, distribution_type) &&
+                        !expired?(profile, min_profile_days_valid) &&
+                        all_services_enabled?(entitlements, entitlements) &&
+                        include_certificate?(profile, certificate)
 
       # profile name needs to be unique
-      if !valid && !profile.nil?
+      unless profile.nil?
         profile.delete!
         ProfileClient.clear_cache(false, platform)
       end
@@ -117,94 +91,60 @@ module Portal
       profile
     end
 
-    def self.matching_profile(profiles, distribution_type, bundle_id, entitlements, portal_certificate, min_profile_days_valid = 0)
-      # filter for distribution type
+    def self.bundle_id_matches?(profile, app)
+      if profile.app.bundle_id != app.bundle_id
+        Log.debug("Profile (#{profile.name}) bundle id: #{profile.app.bundle_id}, should be: #{app.bundle_id}")
+        return false
+      end
+      true
+    end
+
+    def self.distribution_type_matches?(profile, distribution_type)
       distribution_methods = {
         'development' => 'limited',
         'app-store' => 'store',
         'ad-hoc' => 'adhoc',
         'enterprise' => 'inhouse'
       }
-      profiles = profiles.select do |profile|
-        profile.distribution_method == distribution_methods[distribution_type]
-      end
-      Log.debug("#{distribution_type} profiles (#{profiles.length}):")
-      profiles.each do |profile|
-        Log.debug(profile.name)
-      end
+      desired_distribution_method = distribution_methods[distribution_type]
 
-      # Separate matching profiles
-      # full_matching_profiles contains profiles which bundle id equals to the provided bundle_id
-      # matching_profiles contains profiles which bundle id glob matches to the provided bundle_id
-      full_matching_profiles = []
-      matching_profiles = []
-      profiles.each do |profile|
-        if profile.app.bundle_id == bundle_id
-          full_matching_profiles.push(profile)
-          next
-        end
-
-        matching_profiles.push(profile) if File.fnmatch(profile.app.bundle_id, bundle_id)
+      unless profile.distribution_method == desired_distribution_method
+        Log.debug("Profile (#{profile.name}) distribution type: #{profile.distribution_method}, should be: #{desired_distribution_method}")
+        return false
       end
+      false
+    end
 
+    def self.expired?(profile, min_profile_days_valid)
       # Increment the current time with days in seconds (1 day = 86400 secs) the profile has to be valid for
       expire = Time.now + (min_profile_days_valid * 86_400)
 
-      # remove profiles which does not contains all of the provided services (entitlements)
-      # and the profiles which does not contains the provided certificate (portal_certificate)
-      filtered_full_matching_profiles = []
-      full_matching_profiles.each do |profile|
-        if Time.parse(profile.expires.to_s) < expire
-          if min_profile_days_valid > 0
-            Log.debug("Profile (#{profile.name}) matches to target: #{bundle_id}, but it's not valid for #{min_profile_days_valid} days")
-          else
-            Log.debug("Profile (#{profile.name}) matches to target: #{bundle_id}, but expired at: #{profile.expires}")
-          end
-          next
+      if Time.parse(profile.expires.to_s) < expire
+        if min_profile_days_valid > 0
+          Log.debug("Profile (#{profile.name}) is not valid for #{min_profile_days_valid} days")
+        else
+          Log.debug("Profile (#{profile.name}) expired at: #{profile.expires}")
         end
 
-        unless AppClient.all_services_enabled?(profile.app, entitlements)
-          Log.debug("Profile (#{profile.name}) matches to target: #{bundle_id}, but has missing services")
-          next
-        end
-
-        unless include_certificate?(profile, portal_certificate)
-          Log.debug("Profile (#{profile.name}) matches to target: #{bundle_id}, but does not contain the provided certificate")
-          next
-        end
-
-        filtered_full_matching_profiles.push(profile)
+        return true
       end
+      false
+    end
 
-      # prefer the full bundle id match over the glob match
-      return filtered_full_matching_profiles[0] unless filtered_full_matching_profiles.empty?
-
-      filtered_matching_profiles = []
-      matching_profiles.each do |profile|
-        if Time.parse(profile.expires.to_s) < expire
-          if min_profile_days_valid > 0
-            Log.debug("Profile (#{profile.name}) matches to target: #{bundle_id}, but it's not valid for #{min_profile_days_valid} days")
-          else
-            Log.debug("Profile (#{profile.name}) matches to target: #{bundle_id}, but expired at: #{profile.expires}")
-          end
-          next
-        end
-
-        unless AppClient.all_services_enabled?(profile.app, entitlements)
-          Log.debug("Wildcard Profile (#{profile.name}) matches to target: #{bundle_id}, but has missing services")
-          next
-        end
-
-        unless include_certificate?(profile, portal_certificate)
-          Log.debug("Wildcard Profile (#{profile.name}) matches to target: #{bundle_id}, but does not contain the provided certificate")
-          next
-        end
-
-        filtered_matching_profiles.push(profile)
+    def self.all_services_enabled?(profile, entitlements)
+      unless AppClient.all_services_enabled?(profile.app, entitlements)
+        Log.debug("Profile (#{profile.name}) does not contain every required services")
+        return false
       end
+      true
+    end
 
-      filtered_matching_profiles[0] unless filtered_matching_profiles.empty?
-      nil
+    def self.include_certificate?(profile, certificate)
+      profile.certificates.each do |portal_certificate|
+        return true if portal_certificate.id == certificate.id
+      end
+      Log.debug("Profile (#{profile.name}) does not contain certificate: #{certificate.name}")
+      false
     end
 
     def self.clear_cache(xcode_managed, platform)
@@ -241,13 +181,6 @@ module Portal
       @profiles[platform] = platform_profiles
 
       profiles
-    end
-
-    def self.include_certificate?(profile, certificate)
-      profile.certificates.each do |portal_certificate|
-        return true if portal_certificate.id == certificate.id
-      end
-      false
     end
 
     def self.portal_profile_class(distribution_type)
