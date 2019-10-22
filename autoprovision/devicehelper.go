04021d01602fe851768722b4d0b02fa35c33a527
Akos Birmacher <akos.birmacher@bitrise.io>
2019-10-22 11:33:27 +0200

    nil fix

--- autoprovision/devicehelper.go
+++ autoprovision/devicehelper.go
@@ -4,11 +4,18 @@ import "github.com/bitrise-steplib/steps-ios-auto-provision/appstoreconnect"
 
 // ListDevices returns the reigestered devices on the Apple Developer portal
 func ListDevices(client *appstoreconnect.Client, opt *appstoreconnect.ListDevicesOptions) ([]appstoreconnect.Device, error) {
-	nextPageURL := ""
+	var filterUDID string
+	if opt == nil {
+		filterUDID = ""
+	} else {
+		filterUDID = opt.FilterUDID
+	}
+
+	var nextPageURL string
 	var devices []appstoreconnect.Device
 	for {
 		response, err := client.Provisioning.ListDevices(&appstoreconnect.ListDevicesOptions{
-			FilterUDID: opt.FilterUDID,
+			FilterUDID: filterUDID,
 			Limit:      20,
 			Next:       nextPageURL,
 		})
