1fefb1aa7deb541752636264d84052ff6b2d7ea1
Krisztián Gödrei <krisztian.godrei@bitrise.io>
2019-12-19 14:20:58 +0100

    profile name layout update (#104)
    * profile name layout update
    
    * remove unused function and macOS related code

---
+++ autoprovision/profilehelper_test.go
@@ -0,0 +1,84 @@
+package autoprovision
+
+import (
+	"testing"
+
+	"github.com/bitrise-steplib/steps-ios-auto-provision/appstoreconnect"
+)
+
+func Test_profileName(t *testing.T) {
+	tests := []struct {
+		profileType appstoreconnect.ProfileType
+		bundleID    string
+		want        string
+		wantErr     bool
+	}{
+		{
+			profileType: appstoreconnect.IOSAppDevelopment,
+			bundleID:    "io.bitrise.app",
+			want:        "Bitrise iOS development - (io.bitrise.app)",
+			wantErr:     false,
+		},
+		{
+			profileType: appstoreconnect.IOSAppStore,
+			bundleID:    "io.bitrise.app",
+			want:        "Bitrise iOS app-store - (io.bitrise.app)",
+			wantErr:     false,
+		},
+		{
+			profileType: appstoreconnect.IOSAppAdHoc,
+			bundleID:    "io.bitrise.app",
+			want:        "Bitrise iOS ad-hoc - (io.bitrise.app)",
+			wantErr:     false,
+		},
+		{
+			profileType: appstoreconnect.IOSAppInHouse,
+			bundleID:    "io.bitrise.app",
+			want:        "Bitrise iOS enterprise - (io.bitrise.app)",
+			wantErr:     false,
+		},
+
+		{
+			profileType: appstoreconnect.TvOSAppDevelopment,
+			bundleID:    "io.bitrise.app",
+			want:        "Bitrise tvOS development - (io.bitrise.app)",
+			wantErr:     false,
+		},
+		{
+			profileType: appstoreconnect.TvOSAppStore,
+			bundleID:    "io.bitrise.app",
+			want:        "Bitrise tvOS app-store - (io.bitrise.app)",
+			wantErr:     false,
+		},
+		{
+			profileType: appstoreconnect.TvOSAppAdHoc,
+			bundleID:    "io.bitrise.app",
+			want:        "Bitrise tvOS ad-hoc - (io.bitrise.app)",
+			wantErr:     false,
+		},
+		{
+			profileType: appstoreconnect.TvOSAppInHouse,
+			bundleID:    "io.bitrise.app",
+			want:        "Bitrise tvOS enterprise - (io.bitrise.app)",
+			wantErr:     false,
+		},
+		{
+			profileType: appstoreconnect.ProfileType("unknown"),
+			bundleID:    "io.bitrise.app",
+			want:        "",
+			wantErr:     true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(string(tt.profileType), func(t *testing.T) {
+			got, err := profileName(tt.profileType, tt.bundleID)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("profileName() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("profileName() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
