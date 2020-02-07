23a71e5ee3f84e748d4e8a422386d29d5e585745
Áron Szabados <aronszabados@gmail.com>
2020-02-07 18:14:50 +0100

    Added tests

--- certificate_downloader_test.go
+++ certificate_downloader_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"io"
 	"net/http"
 	"testing"
@@ -81,6 +82,7 @@ func TestDownloadCertificates_HttpClientWorking_DownloadSucceeds(t *testing.T) {
 		Body:       mockCloser,
 	}
 	testCertInfo := certificateutil.CertificateInfoModel{}
+	testCertInfoList := []certificateutil.CertificateInfoModel{testCertInfo}
 	mockHTTPClient := new(MockHTTPClient)
 	mockCertificateConverter := new(MockCertificateConverter)
 	mockIOUtils := new(MockIOUtils)
@@ -96,7 +98,7 @@ func TestDownloadCertificates_HttpClientWorking_DownloadSucceeds(t *testing.T) {
 
 	mockHTTPClient.On("Do", mock.Anything).Return(&testHTTPResponse, nil)
 	mockLogger.On("log", mock.AnythingOfType("string"), mock.Anything)
-	mockCertificateConverter.On("CertificatesFromPKCS12Content", mock.Anything, testPass).Return([]certificateutil.CertificateInfoModel{testCertInfo}, nil)
+	mockCertificateConverter.On("CertificatesFromPKCS12Content", mock.Anything, testPass).Return(testCertInfoList, nil)
 	mockCloser.On("Close").Return(nil)
 	mockIOUtils.On("ReadAll", mockCloser).Return([]byte{1, 2, 3}, nil)
 
@@ -106,5 +108,104 @@ func TestDownloadCertificates_HttpClientWorking_DownloadSucceeds(t *testing.T) {
 	// Assert
 	assert.Nil(t, error, "Error should be nil!")
 	assert.NotNil(t, modelList, "Model list should not be nil!")
+	assert.ElementsMatch(t, modelList, testCertInfoList)
 	mockIOUtils.AssertNotCalled(t, "ReadFile", mock.AnythingOfType("string"))
+	mockCloser.AssertCalled(t, "Close")
+}
+
+func TestDownloadCertificates_HttpClient500_DownloadReturnsError(t *testing.T) {
+	// Arrange
+	testPass := "testpass"
+	testURLList := []CertificateFileURL{
+		CertificateFileURL{
+			URL:        "http://test.test",
+			Passphrase: testPass,
+		},
+	}
+	mockCloser := new(MockCloser)
+	testHTTPResponse := http.Response{
+		Status:     "500",
+		StatusCode: 500,
+		Body:       mockCloser,
+	}
+	testCertInfo := certificateutil.CertificateInfoModel{}
+	testCertInfoList := []certificateutil.CertificateInfoModel{testCertInfo}
+	mockHTTPClient := new(MockHTTPClient)
+	mockCertificateConverter := new(MockCertificateConverter)
+	mockIOUtils := new(MockIOUtils)
+	mockLogger := new(MockLogger)
+	certificateDownloader := CertificateDownloaderImpl{
+		httpClient:      mockHTTPClient,
+		debugLogger:     mockLogger.log,
+		warnLogger:      mockLogger.log,
+		pkcs12Converter: mockCertificateConverter.CertificatesFromPKCS12Content,
+		readFile:        mockIOUtils.ReadFile,
+		readAll:         mockIOUtils.ReadAll,
+	}
+
+	mockHTTPClient.On("Do", mock.Anything).Return(&testHTTPResponse, nil)
+	mockLogger.On("log", mock.AnythingOfType("string"), mock.Anything)
+	mockCertificateConverter.On("CertificatesFromPKCS12Content", mock.Anything, testPass).Return(testCertInfoList, nil)
+	mockCloser.On("Close").Return(nil)
+	mockIOUtils.On("ReadAll", mockCloser).Return([]byte{1, 2, 3}, nil)
+
+	// Act
+	modelList, error := certificateDownloader.DownloadCertificates(testURLList)
+
+	// Assert
+	assert.NotNil(t, error, "Error should not be nil!")
+	assert.Nil(t, modelList, "Model list should be nil!")
+	mockCertificateConverter.AssertNotCalled(t, "CertificatesFromPKCS12Content", mock.Anything, mock.Anything)
+	mockIOUtils.AssertNotCalled(t, "ReadFile", mock.AnythingOfType("string"))
+	mockCloser.AssertCalled(t, "Close")
+}
+
+func TestDownloadCertificates_FailedToCloseBody_FailureGetsLogged(t *testing.T) {
+	// Arrange
+	testPass := "testpass"
+	testError := fmt.Errorf("close_error")
+	testURLList := []CertificateFileURL{
+		CertificateFileURL{
+			URL:        "http://test.test",
+			Passphrase: testPass,
+		},
+	}
+	mockCloser := new(MockCloser)
+	testHTTPResponse := http.Response{
+		Status:     "200",
+		StatusCode: 200,
+		Body:       mockCloser,
+	}
+	testCertInfo := certificateutil.CertificateInfoModel{}
+	testCertInfoList := []certificateutil.CertificateInfoModel{testCertInfo}
+	mockHTTPClient := new(MockHTTPClient)
+	mockCertificateConverter := new(MockCertificateConverter)
+	mockIOUtils := new(MockIOUtils)
+	mockDebugLogger := new(MockLogger)
+	mockWarnLogger := new(MockLogger)
+	certificateDownloader := CertificateDownloaderImpl{
+		httpClient:      mockHTTPClient,
+		debugLogger:     mockDebugLogger.log,
+		warnLogger:      mockWarnLogger.log,
+		pkcs12Converter: mockCertificateConverter.CertificatesFromPKCS12Content,
+		readFile:        mockIOUtils.ReadFile,
+		readAll:         mockIOUtils.ReadAll,
+	}
+
+	mockHTTPClient.On("Do", mock.Anything).Return(&testHTTPResponse, nil)
+	mockWarnLogger.On("log", mock.AnythingOfType("string"), mock.MatchedBy(func(input []interface{}) bool {
+		return input[1].(error).Error() == testError.Error()
+	}))
+	mockDebugLogger.On("log", mock.AnythingOfType("string"), mock.Anything)
+	mockCertificateConverter.On("CertificatesFromPKCS12Content", mock.Anything, testPass).Return(testCertInfoList, nil)
+	mockCloser.On("Close").Return(testError)
+	mockIOUtils.On("ReadAll", mockCloser).Return([]byte{1, 2, 3}, nil)
+
+	// Act
+	_, err := certificateDownloader.DownloadCertificates(testURLList)
+
+	// Assert
+	assert.Nil(t, err, "Error should be nil!")
+	mockCloser.AssertCalled(t, "Close")
+	mockWarnLogger.AssertExpectations(t)
 }
