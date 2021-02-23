8fc568ae866a9ac27775a22fdb0c6a7546e8e46f
Laszlo Pusok <laszlo.pusok@bitrise.io>
2021-02-23 11:56:17 +0100

    Add valid device to invalid udid test

--- spec/device_client_spec.rb
+++ spec/device_client_spec.rb
@@ -23,21 +23,25 @@ RSpec.describe '.ensure_test_devices' do
   end
 
   it 'supresses error due to invalid od mac device UDID' do
-    device = Device.new(
+    existing_device = Device.new(
       'device_identifier' => '123456',
-      'title' => 'New Device'
+      'title' => 'Existing Device'
+    )
+    invalid_device = Device.new(
+      'device_identifier' => 'invalid-udid',
+      'title' => 'Invalid Device'
     )
 
     fake_portal_device = double
-    allow(fake_portal_device).to receive(:name).and_return(device.name)
-    allow(fake_portal_device).to receive(:udid).and_return(device.udid)
+    allow(fake_portal_device).to receive(:name).and_return(existing_device.name)
+    allow(fake_portal_device).to receive(:udid).and_return(existing_device.udid)
 
     fake_portal_client = double
-    allow(fake_portal_client).to receive(:all).and_return(nil)
+    allow(fake_portal_client).to receive(:all).and_return([fake_portal_device])
     allow(fake_portal_client).to receive(:create!).and_raise('error')
 
-    valid_devices = Portal::DeviceClient.ensure_test_devices([device], fake_portal_client)
+    valid_devices = Portal::DeviceClient.ensure_test_devices([existing_device, invalid_device], fake_portal_client)
 
-    expect(valid_devices).to eq([])
+    expect(valid_devices).to eq([existing_device])
   end
 end
