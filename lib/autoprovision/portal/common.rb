2878b28c20e402a5948c30493a0165231158fb1a
Krisztián Gödrei <krisztian.godrei@bitrise.io>
2021-07-20 16:36:53 +0200

    Improve exception logging.

--- lib/autoprovision/portal/common.rb
+++ lib/autoprovision/portal/common.rb
@@ -1,15 +1,11 @@
 require 'spaceship'
 
-def result_string(exception)
-  result = exception.preferred_error_info
-  return nil unless result
-  result.join(' ')
+def result_string(ex)
+  ex.preferred_error_info&.join(' ') || ex.to_s
 end
 
 def run_and_handle_portal_function
   yield
 rescue Spaceship::Client::UnexpectedResponse => ex
-  message = result_string(ex)
-  raise ex unless message
-  raise message
+  raise result_string(ex)
 end
